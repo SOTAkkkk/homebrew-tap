57640f1af9b63dbdf6b39c206a7625603c661679
SOTAkkkk <>
2023-09-22 20:55:23 +0900

    fix formula

--- Formula/tutorial-cli-macos.rb
+++ Formula/tutorial-cli-macos.rb
@@ -1,18 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class TutorialCliMacos < Formula
-    desc ""
-    homepage ""
-    url "https://github.com/SOTAkkkk/tutorial-node-cli/releases/download/v1.0.0/tutorial-cli-macos.tar.gz"
-    sha256 "86c2aed3c1c31f80eb2e11b9c4d8ee9a7a560d9b852bc9112af5b63de0b3ba3f"
-    license ""
-    version "1.0.0"
-  
-    def install
-      bin.install "tutorial-cli-macos"
-      system "./configure", *std_configure_args, "--disable-silent-rules"
-    end
-  
-    test do
-      system "false"
-    end
+  desc ""
+  homepage ""
+  url "https://github.com/SOTAkkkk/tutorial-node-cli/releases/download/v1.0.0/tutorial-cli-macos.tar.gz"
+  sha256 "86c2aed3c1c31f80eb2e11b9c4d8ee9a7a560d9b852bc9112af5b63de0b3ba3f"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    bin.install "tutorial-cli"
+    # system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
   end
-  
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test tutorial-cli-macos`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
